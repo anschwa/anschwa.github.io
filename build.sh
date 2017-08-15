5b0e41e5727d378dc8b66d1e8ae230438a70a2d8
Adam Schwartz <adam@anschwa.com>
2017-08-14 21:31:35 -0400

    refactor build.sh

--- build.sh
+++ build.sh
@@ -10,10 +10,9 @@ mkdir $path
 command -v wget > /dev/null 2>&1 || { echo "Do you have wget installed?" >&2; exit 1; }
 
 # Build the static site
-wget --directory-prefix $path --recursive --domains 0:5000 0:5000 --no-host-directories
-wget --directory-prefix $path --recursive --domains 0:5000 0:5000/blog/ --no-host-directories
-wget --directory-prefix $path --recursive --domains 0:5000 0:5000/projects/ --no-host-directories
+pages=("blog/" "projects/" "style.css" "me.jpg")
 
-# fetch static resources
-wget --directory-prefix $path 0:5000/style.css
-wget --directory-prefix $path 0:5000/me.jpg
+for x in "${pages[@]}"
+do
+    wget --directory-prefix $path --recursive --domains 0:5000 "0:5000/$x" --no-host-directories
+done
